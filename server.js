52797ceb64dd5f2b5912c48630c7b3fa72343d06
Atharva <116326781+dark-Repo@users.noreply.github.com>
2025-08-26 22:51:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/api/v1/transections', require('./routes/transectionRoutes'));
 //static files
 app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
 })
 
