94e453c264a617c2f3c4a3ef0ea7f11c496a3b78
Atharva <atharva9421@gmail.com>
2025-08-26 19:51:09 +0530

    de

--- client/src/components/Layouts/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="bg-dark text-light p-4">
-            <h6 className="text-center"> All right reserved &copy; Atharva</h6>
-        </div>
-    )
-}
