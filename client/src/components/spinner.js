fb0b2247ac9d446a14baa98e07c6af4565afe264
Atharva <116326781+dark-Repo@users.noreply.github.com>
2025-08-26 19:50:12 +0530

    Delete client directory

--- client/src/components/spinner.js
+++
@@ -1,7 +0,0 @@
-export default function Spinner() {
-    return (
-        <div className="spinner-border m-5" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-    )
-}
